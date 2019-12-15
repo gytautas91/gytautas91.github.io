fe56829819259a3f183d7c06ec1c66a4257d5720
gytautas <gytautas.pilkauskas@gmail.com>
2019-12-15 15:49:23 +0200

    ns-xms clean up part 1

---
+++ ns-xmas/precache-manifest.53b88b80874eec06939a7ca9bb6852d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f309a01fe0ec4dbe9e00c6cbeef85984",
+    "url": "/ns-xmas/index.html"
+  },
+  {
+    "revision": "635a2c09439299ed8082",
+    "url": "/ns-xmas/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "ba1137440397a9fa9148",
+    "url": "/ns-xmas/static/css/main.69fb2e36.chunk.css"
+  },
+  {
+    "revision": "635a2c09439299ed8082",
+    "url": "/ns-xmas/static/js/2.43a466cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ns-xmas/static/js/2.43a466cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba1137440397a9fa9148",
+    "url": "/ns-xmas/static/js/main.b13747ec.chunk.js"
+  },
+  {
+    "revision": "e6ec9e4c1b41bafa7dc3",
+    "url": "/ns-xmas/static/js/runtime-main.c8ee884b.js"
+  }
+]);
--- ns-xmas/service-worker.js
+++ ns-xmas/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.196a5fa4547b05ba07099d732077fdbe.js"
+  "/ns-xmas/precache-manifest.53b88b80874eec06939a7ca9bb6852d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ns-xmas/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

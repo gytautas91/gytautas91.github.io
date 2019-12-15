0cea3abcd2c557a28e5d15e31d0af7aff9f562ec
gytautas <gytautas.pilkauskas@gmail.com>
2019-12-15 15:39:55 +0200

    ns xmas

---
+++ ns-xmas/precache-manifest.196a5fa4547b05ba07099d732077fdbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f2a0d6b983417cbbf477319b43456e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635a2c09439299ed8082",
+    "url": "/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "177a90bd1ae01c71f710",
+    "url": "/static/css/main.69fb2e36.chunk.css"
+  },
+  {
+    "revision": "635a2c09439299ed8082",
+    "url": "/static/js/2.43a466cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.43a466cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "177a90bd1ae01c71f710",
+    "url": "/static/js/main.6259ed29.chunk.js"
+  },
+  {
+    "revision": "12c429b430bdb09e85eb",
+    "url": "/static/js/runtime-main.9c996d25.js"
+  }
+]);
